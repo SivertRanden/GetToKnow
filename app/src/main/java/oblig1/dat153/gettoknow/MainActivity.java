0dbbed6d08beb55daacee59c0ca038c7a45e02a4
SivertRanden <u4lnYi3I>
2018-01-15 11:06:16 +0100

    Initial commit

---
+++ app/src/main/java/oblig1/dat153/gettoknow/MainActivity.java
@@ -0,0 +1,13 @@
+package oblig1.dat153.gettoknow;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
