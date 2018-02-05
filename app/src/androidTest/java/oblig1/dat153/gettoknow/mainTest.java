4e23a9cc88b2c5998eaea36d2bf1c20d0c5d9979
Bentein <benteinvathnethomassen@gmail.com>
2018-02-05 16:17:27 +0100

    added main activity test

---
+++ app/src/androidTest/java/oblig1/dat153/gettoknow/mainTest.java
@@ -0,0 +1,72 @@
+package oblig1.dat153.gettoknow;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.support.test.espresso.Espresso;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.test.runner.lifecycle.ActivityLifecycleMonitorRegistry;
+import android.support.test.runner.lifecycle.Stage;
+import android.util.Log;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+import oblig1.dat153.gettoknow.activities.MainActivity;
+import oblig1.dat153.gettoknow.model.PersonCollection;
+
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.pressBack;
+import static android.support.test.espresso.action.ViewActions.clearText;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
+
+/**
+ * Created by AndroidMaker on 05-Feb-18.
+ */
+
+@RunWith(AndroidJUnit4.class)
+public class mainTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> addPeopleTestRule = new ActivityTestRule<MainActivity>(MainActivity.class);
+
+    @Test
+    public void activitySwap() {
+        onView(withId(R.id.button1)).perform(click());
+        assertEquals(getActivityInstance().getLocalClassName(), "activities.ListNames");
+        pressBack();
+        onView(withId(R.id.button2)).perform(click());
+        assertEquals(getActivityInstance().getLocalClassName(), "activities.PictureGallery");
+        pressBack();
+        onView(withId(R.id.button3)).perform(click());
+        assertEquals(getActivityInstance().getLocalClassName(), "activities.LearningMode");
+        pressBack();
+        onView(withId(R.id.button4)).perform(click());
+        assertEquals(getActivityInstance().getLocalClassName(), "activities.AddPeople");
+    }
+
+    private Activity getActivityInstance(){
+        final Activity[] currentActivity = {null};
+
+        getInstrumentation().runOnMainSync(new Runnable(){
+            public void run(){
+                Collection<Activity> resumedActivity = ActivityLifecycleMonitorRegistry.getInstance().getActivitiesInStage(Stage.RESUMED);
+                Iterator<Activity> it = resumedActivity.iterator();
+                currentActivity[0] = it.next();
+            }
+        });
+
+        return currentActivity[0];
+    }
+
+
+}
